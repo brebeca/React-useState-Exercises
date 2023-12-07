0fa7edafbda3199a2d760819fb4a6489b52a86d9
Rebeca <rstingu@gannett.com>
2023-12-07 16:38:50 +0200

    color switcher exercise

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import InputField from "./teme/tema3/InputField";
 import CharacterCounter from "./teme/tema4/CharacterCounter";
 import TodoList from "./teme/tema5/ToDoListRezolvare";
 import SearchFilter from "./teme/tema6/SearchFilter";
+import ColorSwitcher from "./teme/tema7/ColorSwitcher";
 
 function App() {
   return (
     <div className="container">
-      <Counter />
+      <ColorSwitcher />
     </div>
   );
 }
