6b73e52bad5845afbfe37fc765aa8e847fe11198
Rebeca <rstingu@gannett.com>
2023-12-07 20:08:55 +0200

    updates

--- src/teme/tema7/ColorSwitcher.js
+++ src/teme/tema7/ColorSwitcher.js
@@ -2,13 +2,17 @@ import React from "react";
 import switcherStyles from "./ColorSwitcher.module.css";
 
 function ColorSwitcher() {
+  let bgColor = "red";
+
   return (
     <div className={switcherStyles.container}>
       <select className={switcherStyles.switcher}>
         {/* Render the color options here */}
       </select>
 
-      <div></div>
+      <div
+        style={{ backgroundColor: bgColor, width: "100px", height: "100px" }}
+      ></div>
     </div>
   );
 }
