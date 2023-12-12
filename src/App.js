00069d2c8484df1b6b9f338b1218fa1c5c1a4314
Rebeca <rstingu@gannett.com>
2023-12-12 20:08:41 +0200

    added nav

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Link } from "react-router-dom";
 
 import Counter from "./teme/tema2/ExercitiuCounter";
 import InputField from "./teme/tema3/InputField";
@@ -6,10 +7,12 @@ import CharacterCounter from "./teme/tema4/CharacterCounter";
 import TodoList from "./teme/tema5/ToDoListRezolvare";
 import SearchFilter from "./teme/tema6/SearchFilterRezolvare";
 import ColorSwitcher from "./teme/tema7/ColorSwitcher";
+import Nav from "./NavBar";
 
 function App() {
   return (
     <div>
+      <Nav />
       <TodoList />
     </div>
   );
